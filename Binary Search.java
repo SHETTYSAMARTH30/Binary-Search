88935d1db6b32bf9ff1939f9b1ead3a40d005a77
SHETTYSAMARTH30 <54606449+SHETTYSAMARTH30@users.noreply.github.com>
2020-12-22 16:22:52 -0800

    Create Binary Search.java

---
+++ Binary Search.java
@@ -0,0 +1,24 @@
+class Binary Search {
+    public int search(int[] nums, int target) {
+        
+        int mid, left = 0;
+        int right = nums.length - 1;
+        
+        while(left <= right){
+            
+            mid = left + (right - left) / 2;
+            
+            if(nums[mid] == target)
+                return mid;
+            
+            if(target < nums[mid])
+                    right = mid - 1;
+            
+            else
+                left = mid + 1;
+        }
+        
+        return -1;
+        
+    }
+}
